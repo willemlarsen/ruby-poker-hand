37ba95fccd0dd449f0ee0e866aa172a8f83b3dfe
Willem Larsen <willemlarsen@gmail.com>
2014-07-08 11:05:46 -0700

    improve test to include strings (i.e, '2H') and pass it

--- lib/poker_hand.rb
+++ lib/poker_hand.rb
@@ -1,5 +1,5 @@
 def straight(card1, card2, card3, card4, card5)
-  hand = [card1, card2, card3, card4, card5]
+  hand = [card1[0].to_i, card2[0].to_i, card3[0].to_i, card4[0].to_i, card5[0].to_i]
   hand.sort.each_cons(2).all? do |x,y|
     y == x + 1
   end
--- spec/poker_hand_spec.rb
+++ spec/poker_hand_spec.rb
@@ -2,7 +2,9 @@ require('rspec')
 require('poker_hand.rb')
 
 describe('straight') do
-  it('returns if a hand of five number cards is a straight') do
-    expect(straight(1, 2, 3, 4, 5)).to eq(true)
+  it('returns if a hand of five cards with number values is a straight') do
+    expect(straight('1H', '2S', '3C', '4H', '5H')).to eq(true)
   end
 end
+
+
